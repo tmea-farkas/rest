9ecd553551b47b55453ba1b1f591e4861840c167
Timea Farkas <145453473+tmea-farkas@users.noreply.github.com>
2024-10-14 13:08:55 +0000

    added post app, serializer and model

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,17 +1,21 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Post
 
-class ProfileSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='ownder.profile.image.url')
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
-        model = Profile
+        model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
-            'name', 'content', 'image', 'is_owner'
+            'title', 'content', 'image', 'profile_id', 
+            'profile_image', 'is_owner'
         ]
